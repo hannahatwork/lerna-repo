41b9bc8374c1cda852e574a281570c5301dfd44d
hmccain <hmccain@vetsfirstchoice.com>
2020-10-01 16:02:19 -0700

    Export react component.

--- packages/test/src/index.js
+++ packages/test/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
+export default ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
