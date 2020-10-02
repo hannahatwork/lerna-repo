6540674a4b9dba20d2d6226524c48a012f73eede
hmccain <hmccain@vetsfirstchoice.com>
2020-10-02 10:39:25 -0700

    Comment.

--- packages/fancy-case/src/constants.js
+++ packages/fancy-case/src/constants.js
@@ -1,4 +1,4 @@
-// export const LOWERCASE_LETTERS = /[a-z]/g;
+// export const LOWERCASE_LETTERS = /[a-z]/g; !!!!!
 
 export const UPPERCASE_LETTERS = /[A-Z]/g;
 export const UPPERCASE_OFFSET = 'A'.charCodeAt(0) - 'a'.charCodeAt(0);
